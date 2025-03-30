deeee56cab67f476c167f8ba055a93ce7cb8172e
TCHANKI Djiwonou <116927060+dtchanki@users.noreply.github.com>
2025-03-30 01:56:11 +0000

    Deployement

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,17 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  
-  reactStrictMode: true,
+  output: "standalone", // Important pour les hébergeurs comme Hostinger
+  reactStrictMode: false, // Désactive le mode strict React
+  images: {
+    domains: ["spendwise.fun"], // Remplace "yourdomain.com" par ton vrai domaine
+  },
+  eslint: {
+    ignoreDuringBuilds: true, // Ignore les erreurs ESLint pendant la construction
+  },
+  typescript: {
+    ignoreBuildErrors: true, // Ignore les erreurs TypeScript pendant la construction
+  },
 };
 
 export default nextConfig;
